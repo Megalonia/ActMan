91dcf98aa50d66275e998be1eeeecffc32031294
Megalonia <moosman863@gmail.com>
2021-06-17 20:03:05 -0500

    clean up repo

--- src/astar.rs
+++ src/astar.rs
@@ -5,8 +5,13 @@ pub fn act_man_astar_gs(game_board: GameBoard) -> Vec<char> {
     let mut frontier: VecDeque<Vec<char>> = VecDeque::new();
     frontier.push_back(vec![]);
     while !frontier.is_empty() {
-
+        println!("++++++++++++++++++++");
+        for x in frontier.clone() {
+            println!("{:?}",x);
+        }
+        println!("++++++++++++++++++++");
         let mut p = frontier.pop_front().unwrap();
+        println!("Path to test {:?}",p);
         let mut sk = transition_function(&mut game_board.clone(), &mut p);
 
         if goal(&mut sk) {
@@ -24,15 +29,25 @@ pub fn act_man_astar_gs(game_board: GameBoard) -> Vec<char> {
                 let front = transition_function(&mut game_board.clone(), &frontier.front().unwrap());
                 let front_cost =  g_star(game_board.clone(),front.clone()) + h_star(front.clone());
                 let cost =  g_star(game_board.clone(),sx.clone()) + h_star(sx.clone());
-
+                println!("new path {:?}",sx.moves_so_far);
+                println!("cost {}",cost);
+                println!("front cost {}",front_cost);
                 if  cost <  front_cost {
                    frontier.push_front(px.clone()); 
                 }else{
                     for (i,path) in frontier.clone().into_iter().enumerate() {
                         let path_board = transition_function(&mut game_board.clone(), &path);
                         let path_cost = g_star(game_board.clone(),path_board.clone()) + h_star(path_board.clone());
+                        println!("{:?}",path_cost);
                         if cost == path_cost && px.len() == path_board.moves_so_far.len() {
                             frontier.insert(i,px.clone());
+                            break;
+                        }else if cost < path_cost {
+                            frontier.insert(i,px.clone());
+                            break;
+                        }else {
+                            frontier.insert(i+1,px.clone());
+                            break;
                         }
                     }
                 }
